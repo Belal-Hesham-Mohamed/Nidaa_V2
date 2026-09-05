ececb76db34bb6a7166df1270a2fb8e58690c8a6
belal_h <compare12121@gmail.com>
2026-09-05 10:13:42 +0300

    Refactor PrayerTimesCubit to include GetPrayerTimesWithCache use case dependency

--- lib/prayer_times/presentation/cubit/prayer_times_cubit.dart
+++ lib/prayer_times/presentation/cubit/prayer_times_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:nidaa_v2/prayer_times/domain/usecase/get_prayer_times_with_cache_usecase.dart';
 
 part 'prayer_times_state.dart';
 
 class PrayerTimesCubit extends Cubit<PrayerTimesState> {
-  PrayerTimesCubit() : super(PrayerTimesInitial());
+  final GetPrayerTimesWithCacheUsecase _getPrayerTimesWithCacheUsecase;
+
+  PrayerTimesCubit(this._getPrayerTimesWithCacheUsecase)
+      : super(PrayerTimesInitial());
 }
