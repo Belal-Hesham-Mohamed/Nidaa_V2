4ae8c9bb8442780304a7b46baf337367b90f36b0
Belal Hesham <compare12121@gmail.com>
2026-09-07 15:05:14 +0300

    Localize current location names for UI

--- lib/location/current_location/data/datasource/location_datasorce.dart
+++ lib/location/current_location/data/datasource/location_datasorce.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:nidaa_v2/location/current_location/data/exception/exception.dart'
@@ -7,49 +8,42 @@ import 'package:nidaa_v2/location/current_location/data/models/location_model.da
 import 'package:geolocator/geolocator.dart' as geo;
 
 class LocationDatasource {
-  Future<LocationModel> getLocationData() async {
-    // 1. Check if location service is enabled
+  Future<LocationModel> getLocationData({Locale? locale}) async {
     final serviceEnabled = await geo.Geolocator.isLocationServiceEnabled();
 
     if (!serviceEnabled) {
       throw LocationServiceDisabledException();
     }
 
-    // 2. Check current permission
     var permission = await geo.Geolocator.checkPermission();
 
-    // 3. Ask for permission if it has not been granted yet
     if (permission == geo.LocationPermission.denied) {
       permission = await geo.Geolocator.requestPermission();
     }
 
-    // 4. User denied permission
     if (permission == geo.LocationPermission.denied) {
       throw LocationPermissionDeniedException();
     }
 
-    // 5. User permanently denied permission
     if (permission == geo.LocationPermission.deniedForever) {
       throw LocationPermissionDeniedForeverException();
     }
 
     try {
-      // 6. Get current location
       final position = await geo.Geolocator.getCurrentPosition();
 
-      // 7. Get address information from coordinates
       Placemark? placemark;
 
       final placemarks = await Geocoding().placemarkFromCoordinates(
         position.latitude,
         position.longitude,
+        locale: locale?.toLanguageTag(),
       );
 
       if (placemarks.isNotEmpty) {
         placemark = placemarks.first;
       }
 
-      // 8. Return location model
       return LocationModel(
         latitude: position.latitude,
         longitude: position.longitude,
