760684011669bfbf80d383e749bb7c7225ea87c9
Belal Hesham <compare12121@gmail.com>
2026-09-07 15:06:21 +0300

    Keep current location use case independent from UI locale

--- lib/location/current_location/domain/usecase/get_location_usecase.dart
+++ lib/location/current_location/domain/usecase/get_location_usecase.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 import 'package:nidaa_v2/core/error/failuer.dart';
 import 'package:nidaa_v2/location/current_location/domain/entities/location.dart';
 import 'package:nidaa_v2/location/current_location/domain/repositories/location_repo_base.dart';
@@ -9,7 +8,7 @@ class GetLocationUsecase {
 
   GetLocationUsecase(this._locationRepository);
 
-  Future<Either<Failure, Location>> call({Locale? locale}) async {
-    return await _locationRepository.getLocation(locale: locale);
+  Future<Either<Failure, Location>> call() async {
+    return await _locationRepository.getLocation();
   }
 }
