5c93c60a849614a0904ae1336059d69a2e8d0aab
belal_h <compare12121@gmail.com>
2026-09-10 11:45:25 +0300

    Enhance QiblaCubit and QiblaState to handle compass events and device heading

--- lib/qibla/presentation/cubit/qibla_cubit.dart
+++ lib/qibla/presentation/cubit/qibla_cubit.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 import 'package:nidaa_v2/location/current_location/domain/entities/location.dart';
 import 'package:nidaa_v2/qibla/domain/usecase/calculate_qibla_bearing_usecase.dart';
 
@@ -7,8 +10,43 @@ part 'qibla_state.dart';
 
 class QiblaCubit extends Cubit<QiblaState> {
   final CalculateQiblaBearingUseCase _calculateQiblaBearingUseCase;
+  StreamSubscription<CompassEvent>? _compassSubscription;
+  double? _qiblaBearing;
+  double? _deviceHeading;
 
-  QiblaCubit(this._calculateQiblaBearingUseCase) : super(QiblaInitial());
+  QiblaCubit(this._calculateQiblaBearingUseCase) : super(QiblaInitial()) {
+    _subscribeToCompass();
+  }
+
+  void _subscribeToCompass() {
+    final compassEvents = FlutterCompass.events;
+    if (compassEvents == null) {
+      emit(QiblaFailure(QiblaErrorKey.compassUnavailable));
+      return;
+    }
+
+    _compassSubscription = compassEvents.listen(
+      _onCompassEvent,
+      onError: (_, __) => emit(QiblaFailure(QiblaErrorKey.compassError)),
+    );
+  }
+
+  void _onCompassEvent(CompassEvent event) {
+    final heading = event.heading;
+    if (heading == null || !heading.isFinite) {
+      emit(QiblaFailure(QiblaErrorKey.headingUnavailable));
+      return;
+    }
+
+    _deviceHeading = heading;
+    final qiblaBearing = _qiblaBearing;
+    if (qiblaBearing != null) {
+      emit(QiblaSuccess(
+        qiblaBearing: qiblaBearing,
+        deviceHeading: heading,
+      ));
+    }
+  }
 
   void calculateQiblaBearing(Location? location) {
     emit(QiblaLoading());
@@ -30,10 +68,19 @@ class QiblaCubit extends Cubit<QiblaState> {
       return;
     }
 
-    final qiblaBearing = _calculateQiblaBearingUseCase(
+    _qiblaBearing = _calculateQiblaBearingUseCase(
       latitude: latitude,
       longitude: longitude,
     );
-    emit(QiblaSuccess(qiblaBearing: qiblaBearing));
+    emit(QiblaSuccess(
+      qiblaBearing: _qiblaBearing!,
+      deviceHeading: _deviceHeading,
+    ));
+  }
+
+  @override
+  Future<void> close() async {
+    await _compassSubscription?.cancel();
+    return super.close();
   }
 }
--- lib/qibla/presentation/cubit/qibla_state.dart
+++ lib/qibla/presentation/cubit/qibla_state.dart
@@ -9,11 +9,21 @@ final class QiblaLoading extends QiblaState {}
 
 final class QiblaSuccess extends QiblaState {
   final double qiblaBearing;
+  final double? deviceHeading;
 
-  QiblaSuccess({required this.qiblaBearing});
+  QiblaSuccess({
+    required this.qiblaBearing,
+    this.deviceHeading,
+  });
 }
 
-enum QiblaErrorKey { locationUnavailable, invalidCoordinates }
+enum QiblaErrorKey {
+  locationUnavailable,
+  invalidCoordinates,
+  compassUnavailable,
+  compassError,
+  headingUnavailable,
+}
 
 final class QiblaFailure extends QiblaState {
   final QiblaErrorKey errorKey;
